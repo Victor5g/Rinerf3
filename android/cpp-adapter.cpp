f80eb2e70219555c54d0b7f2874196d79c2fea8f
Victor5g <vicoe.sales@gmail.com>
2023-07-23 12:52:19 -0300

    Feat: Cimpile iperf3

--- android/cpp-adapter.cpp
+++ android/cpp-adapter.cpp
@@ -1,8 +1,78 @@
 #include <jni.h>
 #include "react-native-rinerf3.h"
+#include "iperf_api.h"
 
 extern "C"
 JNIEXPORT jdouble JNICALL
 Java_com_rinerf3_Rinerf3Module_nativeMultiply(JNIEnv *env, jclass type, jdouble a, jdouble b) {
     return rinerf3::multiply(a, b);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setRole(JNIEnv *env, jclass type, jlong ipt, jchar role) {
+    iperf_set_test_role((struct iperf_test*)ipt, (char)role);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setServerPort(JNIEnv *env, jclass type, jlong ipt, jint server_port) {
+    iperf_set_test_server_port((struct iperf_test*)ipt, server_port);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setReporterInterval(JNIEnv *env, jclass type, jlong ipt, jdouble reporter_interval) {
+    iperf_set_test_reporter_interval((struct iperf_test*)ipt, (double)reporter_interval);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setStatsInterval(JNIEnv *env, jclass type, jlong ipt, jdouble stats_interval) {
+    iperf_set_test_stats_interval((struct iperf_test*)ipt, (double)stats_interval);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setBindAddress(JNIEnv *env, jclass type, jlong ipt, jstring bind_address) {
+    const char *address = env->GetStringUTFChars(bind_address, 0);
+    iperf_set_test_bind_address((struct iperf_test*)ipt, address);
+    env->ReleaseStringUTFChars(bind_address, address);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setReverse(JNIEnv *env, jclass type, jlong ipt, jint reverse) {
+    iperf_set_test_reverse((struct iperf_test*)ipt, reverse);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setNumStreams(JNIEnv *env, jclass type, jlong ipt, jint num_streams) {
+    iperf_set_test_num_streams((struct iperf_test*)ipt, num_streams);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setRate(JNIEnv *env, jclass type, jlong ipt, jlong rate) {
+    iperf_set_test_rate((struct iperf_test*)ipt, (uint64_t)rate);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setBlksize(JNIEnv *env, jclass type, jlong ipt, jint blksize) {
+    iperf_set_test_blksize((struct iperf_test*)ipt, blksize);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setServerHostname(JNIEnv *env, jclass type, jlong ipt, jstring server_hostname) {
+    const char *hostname = env->GetStringUTFChars(server_hostname, 0);
+    iperf_set_test_server_hostname((struct iperf_test*)ipt, hostname);
+    env->ReleaseStringUTFChars(server_hostname, hostname);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setDuration(JNIEnv *env, jclass type, jlong ipt, jint duration) {
+    iperf_set_test_duration((struct iperf_test*)ipt, duration);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setConnectTimeout(JNIEnv *env, jclass type, jlong ipt, jint connect_timeout) {
+    iperf_set_test_connect_timeout((struct iperf_test*)ipt, connect_timeout);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_ipertest_IperfTest_setTos(JNIEnv *env, jclass type, jlong ipt, jint tos) {
+    iperf_set_test_tos((struct iperf_test*)ipt, tos);
 }
--- android/src/main/java/com/rinerf3/Rinerf3Module.java
+++ android/src/main/java/com/rinerf3/Rinerf3Module.java
@@ -27,6 +27,19 @@ public class Rinerf3Module extends ReactContextBaseJavaModule {
   }
 
   private static native double nativeMultiply(double a, double b);
+  private static native void setRole(long ipt, char role);
+  private static native void setServerPort(long ipt, int server_port);
+  private static native void setReporterInterval(long ipt, double reporter_interval);
+  private static native void setStatsInterval(long ipt, double stats_interval);
+  private static native void setBindAddress(long ipt, String bind_address);
+  private static native void setReverse(long ipt, int reverse);
+  private static native void setNumStreams(long ipt, int num_streams);
+  private static native void setRate(long ipt, long rate);
+  private static native void setBlksize(long ipt, int blksize);
+  private static native void setServerHostname(long ipt, String server_hostname);
+  private static native void setDuration(long ipt, int duration);
+  private static native void setConnectTimeout(long ipt, int connect_timeout);
+  private static native void setTos(long ipt, int tos);
 
   // Example method
   // See https://reactnative.dev/docs/native-modules-android
@@ -34,4 +47,70 @@ public class Rinerf3Module extends ReactContextBaseJavaModule {
   public void multiply(double a, double b, Promise promise) {
     promise.resolve(nativeMultiply(a, b));
   }
+
+@ReactMethod
+    public void setIperfRole(long ipt, char role) {
+        setRole(ipt, role);
+    }
+
+    @ReactMethod
+    public void setIperfServerPort(long ipt, int server_port) {
+        setServerPort(ipt, server_port);
+    }
+
+    @ReactMethod
+    public void setIperfReporterInterval(long ipt, double reporter_interval) {
+        setReporterInterval(ipt, reporter_interval);
+    }
+
+    @ReactMethod
+    public void setIperfStatsInterval(long ipt, double stats_interval) {
+        setStatsInterval(ipt, stats_interval);
+    }
+
+    @ReactMethod
+    public void setIperfBindAddress(long ipt, String bind_address) {
+        setBindAddress(ipt, bind_address);
+    }
+
+    @ReactMethod
+    public void setIperfReverse(long ipt, int reverse) {
+        setReverse(ipt, reverse);
+    }
+
+    @ReactMethod
+    public void setIperfNumStreams(long ipt, int num_streams) {
+        setNumStreams(ipt, num_streams);
+    }
+
+    @ReactMethod
+    public void setIperfRate(long ipt, long rate) {
+        setRate(ipt, rate);
+    }
+
+    @ReactMethod
+    public void setIperfBlksize(long ipt, int blksize) {
+        setBlksize(ipt, blksize);
+    }
+
+    @ReactMethod
+    public void setIperfServerHostname(long ipt, String server_hostname) {
+        setServerHostname(ipt, server_hostname);
+    }
+
+    @ReactMethod
+    public void setIperfDuration(long ipt, int duration) {
+        setDuration(ipt, duration);
+    }
+
+    @ReactMethod
+    public void setIperfConnectTimeout(long ipt, int connect_timeout) {
+        setConnectTimeout(ipt, connect_timeout);
+    }
+
+    @ReactMethod
+    public void setIperfTos(long ipt, int tos) {
+        setTos(ipt, tos);
+    }
+
 }
